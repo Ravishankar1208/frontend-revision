300a05d1744f6becd04d5b5062afdaa2b5c49f95
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-03 22:00:58 +0530

    javascript revision day 1

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+//  alert()
+
+
+// var a = 10
+// let b = 10;
+// const c =  10;
+
+// a = 12;
+// b = 13;
+// c = 14
+ 
+
+// console.log("heloo ravi");
+// console.error("heloo ravi");
+// console.warn("heloo ravi");
+
+
+// let r = prompt("enter name")
+
+// console.log(r);
+
+// alert("not take rest")
+
+"hello bhai kaise ho".split(' ')
