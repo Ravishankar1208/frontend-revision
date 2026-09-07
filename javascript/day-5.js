fbad7cea95bba0553eb7cc9ad038f049d93a46bf
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-07 15:31:53 +0530

    day-5 add

---
+++ javascript/day-5.js
@@ -0,0 +1,25 @@
+//   operator
+
+//   arithmetic operator
+
+//  assignment operator
+        
+//  comparison operator
+
+//  logical operator
+
+// ternary operator
+
+// type checking operator
+ 
+// string operator
+
+// spread operator  
+
+// nullish coalescing operator
+
+// optional chaining operator
+
+//  variable hosting in js
+
+  
