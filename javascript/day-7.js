5bb1d2b37039f259c4160482da416913fffb0081
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-08 18:59:40 +0530

    some added

--- javascript/day-7.js
+++ javascript/day-7.js
@@ -1,56 +1,56 @@
-// //  loop
+// // //  loop
 
-// //conditional statement
+// // //conditional statement
+
+
+// // // let a = 44
+// // // if(a>29){
+// // //   console.log("a is greater");
+  
+// // // }
+// // // else{
+// // //   console.log("a is smaller");
+  
+// // // } 
 
 
 // // let a = 44
-// // if(a>29){
+// // if(29){  //true
 // //   console.log("a is greater");
   
 // // }
 // // else{
 // //   console.log("a is smaller");
   
-// // } 
-
-
-// let a = 44
-// if(29){  //true
-//   console.log("a is greater");
-  
-// }
-// else{
-//   console.log("a is smaller");
-  
-// }
+// // }
  
  
 
 
 
- //   loop
+//  //   loop
 
-//  two types
+// //  two types
 
- // straightforwRD loop
+//  // straightforwRD loop
 
- // dynamic loop
-
-
-// for loop
+//  // dynamic loop
 
 
-// for(let i = 0; i<10; i++){
+// // for loop
+
+
+// // for(let i = 0; i<10; i++){
+// //   console.log(i);
+  
+// // }
+
+
+
+//  for(let i = 0; i<20; i++){
+//   if(i = 10){
+//     continue;
+//   }
 //   console.log(i);
   
-// }
-
-
-
- for(let i = 0; i<20; i++){
-  if(i = 10){
-    continue;
-  }
-  console.log(i);
-  
- }
+//  }
---
+++ javascript/day-8.js
@@ -0,0 +1,84 @@
+// loop question
+
+// level 1- pure begineers pratice
+
+
+// for (let i = 1 ; i<=10; i++){
+//     console.log(i);
+    
+//   }
+
+
+
+// for (let i = 1 ; i<=20; i++){
+
+//   if(i%2 === 0){
+//     console.log(i);
+    
+//   }
+// }
+
+
+
+
+
+// for (let i = 1 ; i<=10; i++){
+//   console.log(i/2);
+  
+// }
+
+
+// for (let i = 10 ; i>=1; i--){
+//   console.log(i);
+  
+// }
+
+
+
+
+// for (let i = 1 ; i<=5; i++){
+//   console.log("yes");
+  
+// }
+
+
+
+// for (let i = 1 ; i<=20; i++){
+  
+//   if(i%2===0){
+//     console.log("even number "+i);
+    
+//   }
+//   else{
+//     console.log("odd number "+i);
+    
+//   }
+  
+// }
+
+
+
+
+
+
+// let prompt = require("prompt-sync")();
+// let num = Number( prompt("enter a number"))
+  
+//   if(num>0){
+//        console.log(`${num} number is  positive`);
+
+    
+//   }
+//   else if(num<0){
+//     console.log(`${num} number is  negative`);
+    
+//   }
+//   else console.log(`${num} is zero`);
+  
+
+
+
+
+
+
+
