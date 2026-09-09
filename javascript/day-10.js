5be2f7be74d18d353f4c8aeb298e81475cda11fa
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-09 15:45:30 +0530

    ..

---
+++ javascript/day-10.js
@@ -0,0 +1,60 @@
+
+// let att = 0;
+
+// let open = false
+// let pass = "ravi"
+// let password = prompt("enter your password: ")
+// if (password === pass) open = true;
+// att++
+
+// while (password !== pass) {
+//   if (att === 3) {
+//     console.error("account lockeed");
+//     break;
+//   }
+//   password = prompt("enter your password: ")
+//   att++
+// }
+// if (open === true) console.log("account opened");
+
+
+
+
+
+
+
+// for(let i = 1; i<=50; i++){
+//   if(i%7===0){
+//     console.log(i);
+    
+//   }
+// }
+
+
+
+
+
+
+// let sum = 0;
+
+// for(let i = 0; i<=30;i++){
+
+//   if(i%2!==0){
+//     sum = sum + i
+//   }
+// }
+
+// console.log(`total sum of number is ${sum}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
