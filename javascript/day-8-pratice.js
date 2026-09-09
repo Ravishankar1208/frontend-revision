706bdc444761b5a39c17849f52e95e3ff3bc8181
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-09 13:30:00 +0530

    ..

--- javascript/day-8-pratice.js
+++ javascript/day-8-pratice.js
@@ -18,7 +18,7 @@
 // Use loop to print 5 × 1 to 5 × 10.
 
 
-for(let i =1 ; i<=10; i++){
-  console.log("5 * "+i+" = "+(5*i));
+// for(let i =1 ; i<=10; i++){
+//   console.log("5 * "+i+" = "+(5*i));
    
-}
+// }
---
+++ javascript/day-9.js
@@ -0,0 +1,84 @@
+// // let age = prompt("please enter your age: ")
+
+// // if(age >=18) console.log("you are eligiblefor vote");
+
+// // else if(age===null) console.log("please enter age");
+
+// // else if(age.trim()=== "") console.log("please enter some thing");
+
+// // else if(console.log("not eligiblefor vote"));
+
+// // else {
+// //   age = Number(age);
+
+// //   if(isNaN(age)){
+// //     console.warn("please enter vaid number");
+
+// //   }
+// // }
+
+// //       while loop
+
+
+
+// // let i = 1
+
+// // while (i<=10) {
+// //   console.log("ravi");
+// //  i++
+
+// // }
+
+
+
+
+
+// // let pass = prompt("Enter your password: ");
+
+// // while (pass.trim() !== "stop") {
+// //     console.log(pass);
+
+// //     pass = prompt("Enter your password: ");
+// // }
+
+
+// let att = 0;
+
+// let open = false
+
+// let pass = "ravi"
+
+// let password = prompt("enter your password: ")
+
+// if (password === pass) open = true;
+
+// att++
+
+// while (password !== pass) {
+//   if (att === 3) {
+
+//     console.log("account lockeed");
+//     break;
+
+//   }
+
+//   password = prompt("enter your password: ")
+
+//   att++
+// }
+
+// if (open === true) console.log("account opened");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
