734f0ff117a3567346afd3bc7c1a22a5286dc943
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-09 18:14:19 +0530

    ..

---
+++ javascript/day-11.js
@@ -0,0 +1,130 @@
+// // let counter =0;
+
+// // for(let i = 1; i<=20; i++){
+
+// //   if(counter===3) break;
+
+// //   if(i%2!==0){
+// //     console.log(i);
+// //     counter++;
+    
+// //   }
+
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // let counter = 0;
+
+// // for(let i =1; i<=5; i++){
+// //   let num = +prompt("enter number")
+
+// //   if(num >= 0) counter++
+  
+// // }
+
+// // console.log(counter);
+
+
+
+
+// // let counter = 0
+// // let balance = 1000 
+// // while(balance>0){
+
+// //   if(counter===3) break;
+// //   let bal = +prompt("enter withdrawal ammount")
+
+// //   balance = balance - bal
+// //   counter++
+// // }
+
+// // if(balance>0) console.log(balance);
+
+// // else console.log(`your balance is low ${balance}`);
+
+
+
+
+// // do while loop
+
+
+// let i = 1
+// do{
+//   console.log("hyyy");
+//   i++
+  
+// }while (i<=10)
+
+
+
+
+//          recursion
+
+
+// function  abcd() {
+
+//   console.log("hyyy");
+
+//   abcd()
+  
+// }
+
+// abcd()
+
+
+
+
+
+
+//     break statement
+
+
+
+
+
+
+// function 
+// 
+
+
+
+
+// function food() {
+
+
+//   console.log("khana kha naa rahe ");
+  
+  
+// } 
+
+
+// food()
+
+
+
+
+
+// function dosomething(name , email , age){
+
+//   console.log(name , age , email);
+  
+
+// }
+
+// dosomething("ravi" , "ddh@gmail.com", 99)
+
+
+
+
+
