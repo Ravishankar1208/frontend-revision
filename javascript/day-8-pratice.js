3db7407b79885a9c6fc915906f35aeb337e10935
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-09 11:42:36 +0530

    added

---
+++ javascript/day-8-pratice.js
@@ -0,0 +1,24 @@
+// // Level 1 – Pure Beginner Practice
+
+// // 7. Ask user’s age and check if eligible to vote
+// // If age >= 18 → “Eligible”, else → “Not eligible”
+
+
+// let age = prompt("please enter your age: ")
+ 
+// if(age>=18) console.log("you are eligiblefor vote");
+
+// else console.log("not eligiblefor vote");
+
+
+
+
+
+// 8. Print multiplication table of 5
+// Use loop to print 5 × 1 to 5 × 10.
+
+
+for(let i =1 ; i<=10; i++){
+  console.log("5 * "+i+" = "+(5*i));
+   
+}
