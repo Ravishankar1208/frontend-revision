780d3cff34441408f448fae49defbd93bf954809
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-10 23:12:26 +0530

    ..

---
+++ javascript/day-13.js
@@ -0,0 +1,25 @@
+// //pure function 
+// // // impure function
+// //  fcf 
+
+// //
+
+
+
+
+// //    arrays = ek se jayda member rakhne ka jagah
+
+// let arr =[1,23,3,4,5,67,8,8,9];
+
+  
+
+ 
+//  object
+
+let std = {
+  name: "ravi",
+  age: 55
+}
+
+console.log(std);
+let std2 = new Object()
