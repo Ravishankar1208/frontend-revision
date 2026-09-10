c0df1f63def3d8fab27a6d6f7fde74637623c73e
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-10 17:18:28 +0530

    ...

---
+++ javascript/day-12.js
@@ -0,0 +1,40 @@
+//  //   understanding function in js
+
+//   function aalo(age){
+
+//     console.log("aalooooo");
+    
+//   }
+
+//   aalo(44)
+
+
+
+//  parameter 
+
+
+
+
+// function student(name , age){
+//   console.log(name, age );
+  
+// }
+
+// student("ravi", 99 )
+
+
+
+
+
+
+
+// function abcd(a ,b,c,d){
+
+//   console.log(a,b,c,d);
+  
+
+// }
+
+// abcd(  )
+
+
