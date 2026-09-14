ab3136a2a43832516873c8aec66e61f6f0185507
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-14 05:58:33 +0530

    ..

---
+++ javascript/day-15.js
@@ -0,0 +1,75 @@
+//  DOM 
+
+// document object model
+
+// dom ki piller = 4
+
+// 1.   selection of an Element
+// 2.   hanging of html
+// 3.   changing of css
+// 4.   Event listener
+
+
+
+
+//  document .quearySelecotor
+
+// var h1= document.querySelector('h1')
+// var h2= document.querySelector('h2')
+
+// console.log(h1);
+// console.log(h2);
+
+
+
+
+// var h1= document.querySelector('h1')
+// h1.innerHTML = ('changing')
+
+
+
+
+
+// var h1= document.querySelector('h1')
+// h1.innerHTML = ('changing')
+// h1.style.color= 'red'
+// h1.style.backgroundColor='pink'
+
+
+
+
+
+// var h1= document.querySelector('h1')
+// h1.innerHTML = ('changing')
+// h1.style.color= 'red'
+// h1.style.backgroundColor='pink'
+
+
+
+// let box = document.querySelector('#box')
+// box.innerHTML = 'heyy heyy'
+// box.style.backgroundColor ='royalblue'
+
+
+
+
+
+
+
+
+
+
+//  event listener 
+
+// var h1= document.querySelector('h1')
+// h1.addEventListener('click',function () {
+//   console.log("hellow");
+  
+  
+// })
+
+
+
+
+
+
