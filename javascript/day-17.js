d8cc6b514c778195c910c0228725e4d1d08b3b3e
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-15 02:29:42 +0530

    ..

---
+++ javascript/day-17.js
@@ -0,0 +1,63 @@
+// create element
+
+// let h = document.createElement('h1')
+// console.log(h);
+
+
+
+
+
+
+// let btn = document.querySelector('button')
+
+// btn.addEventListener('click', function(){
+//   let h1 = document.createElement("h1")
+// h1.innerHTML= 'hellow world'
+//   console.log(h1);
+  
+  
+// })
+
+
+
+
+
+// var h1 = document.createElement('h1')
+// h1.innerHTML = "hellow ravi"
+// let main = document.querySelector('main')
+// main.appendChild(h1)
+// h1.style.color = 'white'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var h1 = document.createElement('h1')
+h1.innerHTML= "hello ravi"
+let maa = document.querySelector('main')
+maa.appendChild(h1)
