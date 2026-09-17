767c5c2fed75162af8b2629093d620cb6d38f134
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-18 00:29:22 +0530

    ..

---
+++ javascript/day-19.js
@@ -0,0 +1,12 @@
+let card = document.querySelector('img')
+let love = document.querySelector('#love')
+
+
+let like = card.addEventListener('dblclick', function(){
+  love.style.color = "red"  
+  love.style.opacity = 1
+  let pop = setTimeout(() => {
+    alert('You like Aliya jaan ❤️');
+  }, 2000);
+
+})
