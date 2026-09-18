d142cd67e718812122a35695b62b9a07f2a60b3a
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-19 02:17:10 +0530

    ..

---
+++ javascript/day-23.js
@@ -0,0 +1,60 @@
+// var elem = document.querySelectorAll('.elem')
+
+// elem.forEach(function (elem) {
+//       elem.childNodes[3].addEventListener('click', function(){
+        
+//         elem.innerHTML = "Remove"
+//       })
+  
+  
+// })
+
+
+
+// let btn = document.querySelectorAll('button')
+
+// btn.forEach(function(elem){
+
+//   btn.addEventListener('click', function(elem){
+  
+//      elem.innerHTML = 'remove friend'
+//   })
+  
+  
+// })
+
+
+// let btn = document.querySelectorAll('button')
+
+// btn.forEach(function(elem) {
+
+//   elem.addEventListener('click', function() {
+
+//     elem.innerHTML = 'remove friend'
+
+//   })
+
+// })
+
+
+
+
+
+
+let btn = document.querySelectorAll('button')
+
+btn.forEach(function(elem){
+
+  elem.addEventListener('click', function(){
+  
+     if(elem.innerHTML === 'Add him')  {
+      elem.innerHTML = 'remove friend'
+     }
+
+     else  {
+      elem.innerHTML = 'Add him'
+     }
+  })
+  
+  
+})
