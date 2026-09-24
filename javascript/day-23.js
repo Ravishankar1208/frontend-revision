b79bbcd477d187e5e0d9ce8cab80628c15cd6a84
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-25 01:25:25 +0530

    ..

--- javascript/day-23.js
+++ javascript/day-23.js
@@ -1,13 +1,43 @@
-// var elem = document.querySelectorAll('.elem')
+// // var elem = document.querySelectorAll('.elem')
 
-// elem.forEach(function (elem) {
-//       elem.childNodes[3].addEventListener('click', function(){
+// // elem.forEach(function (elem) {
+// //       elem.childNodes[3].addEventListener('click', function(){
         
-//         elem.innerHTML = "Remove"
-//       })
+// //         elem.innerHTML = "Remove"
+// //       })
   
   
-// })
+// // })
+
+
+
+// // let btn = document.querySelectorAll('button')
+
+// // btn.forEach(function(elem){
+
+// //   btn.addEventListener('click', function(elem){
+  
+// //      elem.innerHTML = 'remove friend'
+// //   })
+  
+  
+// // })
+
+
+// // let btn = document.querySelectorAll('button')
+
+// // btn.forEach(function(elem) {
+
+// //   elem.addEventListener('click', function() {
+
+// //     elem.innerHTML = 'remove friend'
+
+// //   })
+
+// // })
+
+
+
 
 
 
@@ -15,46 +45,16 @@
 
 // btn.forEach(function(elem){
 
-//   btn.addEventListener('click', function(elem){
+//   elem.addEventListener('click', function(){
   
-//      elem.innerHTML = 'remove friend'
+//      if(elem.innerHTML === 'Add him')  {
+//       elem.innerHTML = 'remove friend'
+//      }
+
+//      else  {
+//       elem.innerHTML = 'Add him'
+//      }
 //   })
   
   
 // })
-
-
-// let btn = document.querySelectorAll('button')
-
-// btn.forEach(function(elem) {
-
-//   elem.addEventListener('click', function() {
-
-//     elem.innerHTML = 'remove friend'
-
-//   })
-
-// })
-
-
-
-
-
-
-let btn = document.querySelectorAll('button')
-
-btn.forEach(function(elem){
-
-  elem.addEventListener('click', function(){
-  
-     if(elem.innerHTML === 'Add him')  {
-      elem.innerHTML = 'remove friend'
-     }
-
-     else  {
-      elem.innerHTML = 'Add him'
-     }
-  })
-  
-  
-})
