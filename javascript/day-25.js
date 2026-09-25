c322ce75dd7cb16d580d443eb0977bc5f18d77cf
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-26 04:25:07 +0530

    ..

--- javascript/day-25.js
+++ javascript/day-25.js
@@ -1,15 +1,50 @@
+// // // advance js
+
+// // // oops :- 
+
+
+
+// // class remote{
+// //   constructor(){
+
+// //     this.product = "daikin";
+// //     this.price = 12300;
+// //     this.color = "white";
+// //   }
+// //   poweron(){
+// //     console.log("this machine is on now");
+    
+// //   }
+// //   poweroff(){
+// //     console.log("this macine is off now");
+    
+// //   }
+// // }
+
+
+// // let Remote = new remote ()
+
+
+
+
+
+
+
+
+
+
 // // advance js
 
 // // oops :- 
 
 
 
-// class remote{
-//   constructor(){
+// class Remote{
+//   constructor(product, price, color){
 
-//     this.product = "daikin";
-//     this.price = 12300;
-//     this.color = "white";
+//     this.product = product;
+//     this.price = price;
+//     this.color = color;
 //   }
 //   poweron(){
 //     console.log("this machine is on now");
@@ -22,44 +57,9 @@
 // }
 
 
-// let Remote = new remote ()
-
-
-
-
-
-
-
-
-
-
-// advance js
-
-// oops :- 
-
-
-
-class Remote{
-  constructor(product, price, color){
-
-    this.product = product;
-    this.price = price;
-    this.color = color;
-  }
-  poweron(){
-    console.log("this machine is on now");
-    
-  }
-  poweroff(){
-    console.log("this macine is off now");
-    
-  }
-}
-
-
-let remote1 = new Remote ("car", 3333,"black")
-let remote2 = new Remote ("car", 3333,"black")
-let remote3= new Remote ("car", 3333,"black")
-let remote4= new Remote ("car", 3333,"black")
+// let remote1 = new Remote ("car", 3333,"black")
+// let remote2 = new Remote ("car", 3333,"black")
+// let remote3= new Remote ("car", 3333,"black")
+// let remote4= new Remote ("car", 3333,"black")
 
 
