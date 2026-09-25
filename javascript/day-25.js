9f2dab00c0aea5e6631050b2d505c995621f0ef5
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-26 04:23:35 +0530

    ..

---
+++ javascript/day-25.js
@@ -0,0 +1,65 @@
+// // advance js
+
+// // oops :- 
+
+
+
+// class remote{
+//   constructor(){
+
+//     this.product = "daikin";
+//     this.price = 12300;
+//     this.color = "white";
+//   }
+//   poweron(){
+//     console.log("this machine is on now");
+    
+//   }
+//   poweroff(){
+//     console.log("this macine is off now");
+    
+//   }
+// }
+
+
+// let Remote = new remote ()
+
+
+
+
+
+
+
+
+
+
+// advance js
+
+// oops :- 
+
+
+
+class Remote{
+  constructor(product, price, color){
+
+    this.product = product;
+    this.price = price;
+    this.color = color;
+  }
+  poweron(){
+    console.log("this machine is on now");
+    
+  }
+  poweroff(){
+    console.log("this macine is off now");
+    
+  }
+}
+
+
+let remote1 = new Remote ("car", 3333,"black")
+let remote2 = new Remote ("car", 3333,"black")
+let remote3= new Remote ("car", 3333,"black")
+let remote4= new Remote ("car", 3333,"black")
+
+
