b40a8e2a7ef5606e171984a07d9ffe4f9a383924
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-09-26 05:47:44 +0530

    ..

--- javascript/day-26.js
+++ javascript/day-26.js
@@ -1,12 +1,12 @@
-class user{
-  constructor(name, age, village){
-    this.name = name;
-    this.age = age;
-    this.village = village;
-  }
+// class user{
+//   constructor(name, age, village){
+//     this.name = name;
+//     this.age = age;
+//     this.village = village;
+//   }
 
-}
+// }
 
-user.prototype.email = "user@gmail.com";
+// user.prototype.email = "user@gmail.com";
 
-let user1 = new user ("ravi", 21, "mathahi")
+// let user1 = new user ("ravi", 21, "mathahi")
